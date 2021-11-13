70530ad620c9c9c6d1ad45c3a1e5c9e7aac95469
“Srujan03076” <srujanmahaja5@gmail.com>
2021-11-13 22:16:40 +0530

    [Srujan]Added Welcome Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PayrollServicesUsingADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Payroll Services ADO!");
+        }
+    }
+}
