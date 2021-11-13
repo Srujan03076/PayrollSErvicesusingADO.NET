aa6bd983f136c2db545511bd011939b3afb1931c
“Srujan03076” <srujanmahaja5@gmail.com>
2021-11-13 23:46:50 +0530

    [Srujan]Added code retrieve all employee details from databse

--- EmployeeRepository.cs
+++ EmployeeRepository.cs
@@ -12,5 +12,63 @@ namespace PayrollServicesUsingADO.NET
 
         //passing the string to sqlconnection to make connection 
         SqlConnection sqlconnection = new SqlConnection(connectionString);
+
+
+        public void GetAllEmployee()
+        {
+            try
+            {
+                //Creating object for employeemodel and access the fields
+                EmployeeModel employeeModel = new EmployeeModel();
+
+                string query = @"SELECT * FROM employee_payroll;";
+
+                // define sql command object
+                SqlCommand sqlCommand = new SqlCommand(query, sqlconnection);
+
+                //Open the connection
+                this.sqlconnection.Open();
+
+
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        employeeModel.EmployeeId = reader.GetInt32(0);
+                        employeeModel.EmployeeName = reader["EmployeeName"].ToString();
+                        employeeModel.BasicPay = Convert.ToDouble(reader["BasicPay"]);
+                        employeeModel.Deductions = Convert.ToDouble(reader["Deductions"]);
+                        employeeModel.IncomeTax = Convert.ToDouble(reader["IncomeTax"]);
+                        employeeModel.TaxablePay = Convert.ToDouble(reader["TaxablePay"]);
+                        employeeModel.NetPay = Convert.ToDouble(reader["NetPay"]);
+                        employeeModel.Gender = reader["Gender"].ToString();
+                        employeeModel.PhoneNumber = Convert.ToInt64(reader["PhoneNumber"]);
+                        employeeModel.Department = reader["Department"].ToString();
+                        employeeModel.Address = reader["Address"].ToString();
+                        employeeModel.StartDate = Convert.ToDateTime(reader["StartDate"]);
+
+                        Console.WriteLine("{0} {1} {2}  {3} {4} {5}  {6}  {7} {8} {9} {10} {11}", employeeModel.EmployeeId, employeeModel.EmployeeName, employeeModel.BasicPay, employeeModel.StartDate, employeeModel.Gender, employeeModel.Department, employeeModel.PhoneNumber, employeeModel.Address, employeeModel.Deductions, employeeModel.TaxablePay, employeeModel.IncomeTax, employeeModel.NetPay);
+                        Console.WriteLine("\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No data found");
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                this.sqlconnection.Close();
+            }
+        }
     }
+
 }
+  
--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace PayrollServicesUsingADO.NET
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Payroll Services ADO.NET!");
+            EmployeeRepository repository = new EmployeeRepository();
+            Console.WriteLine("Id Name BasicPay Startdate Gender Department PhoneNumber Address Deduction Tax IncomeTax NetPay \n");
+            repository.GetAllEmployee();
+            Console.ReadLine();
+
         }
     }
 }
